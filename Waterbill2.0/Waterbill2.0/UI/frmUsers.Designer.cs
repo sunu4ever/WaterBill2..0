8f56b041b3a933412b8eb7bc4deec3fe3a249aab
sunu4ever <mbabusunu@gmail.com>
2017-05-21 14:55:40 +0530

    user master updated

--- Waterbill2.0/Waterbill2.0/UI/frmUsers.Designer.cs
+++ Waterbill2.0/Waterbill2.0/UI/frmUsers.Designer.cs
@@ -28,15 +28,251 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn1 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn2 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn3 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn4 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition1 = new Telerik.WinControls.UI.TableViewDefinition();
             this.office2010BlueTheme1 = new Telerik.WinControls.Themes.Office2010BlueTheme();
+            this.btnSave = new Telerik.WinControls.UI.RadButton();
+            this.ddlRole = new Telerik.WinControls.UI.RadDropDownList();
+            this.txtPassword = new Telerik.WinControls.UI.RadTextBox();
+            this.txtLoginid = new Telerik.WinControls.UI.RadTextBox();
+            this.btnNew = new Telerik.WinControls.UI.RadButton();
+            this.ddlActive = new Telerik.WinControls.UI.RadDropDownList();
+            this.rlbRole = new Telerik.WinControls.UI.RadLabel();
+            this.rlbActive = new Telerik.WinControls.UI.RadLabel();
+            this.rlbLoginId = new Telerik.WinControls.UI.RadLabel();
+            this.txtFname = new Telerik.WinControls.UI.RadTextBox();
+            this.rlbPwd = new Telerik.WinControls.UI.RadLabel();
+            this.office2010BlueTheme2 = new Telerik.WinControls.Themes.Office2010BlueTheme();
+            this.rlbFname = new Telerik.WinControls.UI.RadLabel();
+            this.radGb2 = new Telerik.WinControls.UI.RadGroupBox();
+            this.rgvUsers = new Telerik.WinControls.UI.RadGridView();
+            this.radGb1 = new Telerik.WinControls.UI.RadGroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ddlRole)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtLoginid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnNew)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ddlActive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rlbRole)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rlbActive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rlbLoginId)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFname)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rlbPwd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rlbFname)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGb2)).BeginInit();
+            this.radGb2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rgvUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGb1)).BeginInit();
+            this.radGb1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold);
+            this.btnSave.ForeColor = System.Drawing.Color.Teal;
+            this.btnSave.Location = new System.Drawing.Point(208, 255);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(103, 31);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            // 
+            // ddlRole
+            // 
+            this.ddlRole.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold);
+            this.ddlRole.Location = new System.Drawing.Point(161, 156);
+            this.ddlRole.Name = "ddlRole";
+            this.ddlRole.Size = new System.Drawing.Size(198, 24);
+            this.ddlRole.TabIndex = 12;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.AutoSize = false;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 10.25F);
+            this.txtPassword.Location = new System.Drawing.Point(161, 116);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(198, 24);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // txtLoginid
+            // 
+            this.txtLoginid.AutoSize = false;
+            this.txtLoginid.Font = new System.Drawing.Font("Segoe UI", 10.25F);
+            this.txtLoginid.Location = new System.Drawing.Point(161, 73);
+            this.txtLoginid.Multiline = true;
+            this.txtLoginid.Name = "txtLoginid";
+            this.txtLoginid.Size = new System.Drawing.Size(198, 24);
+            this.txtLoginid.TabIndex = 2;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold);
+            this.btnNew.ForeColor = System.Drawing.Color.Teal;
+            this.btnNew.Location = new System.Drawing.Point(99, 255);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(103, 31);
+            this.btnNew.TabIndex = 10;
+            this.btnNew.Text = "New";
+            // 
+            // ddlActive
+            // 
+            this.ddlActive.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold);
+            this.ddlActive.Location = new System.Drawing.Point(161, 198);
+            this.ddlActive.Name = "ddlActive";
+            this.ddlActive.Size = new System.Drawing.Size(198, 24);
+            this.ddlActive.TabIndex = 9;
+            // 
+            // rlbRole
+            // 
+            this.rlbRole.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rlbRole.ForeColor = System.Drawing.Color.Teal;
+            this.rlbRole.Location = new System.Drawing.Point(51, 156);
+            this.rlbRole.Name = "rlbRole";
+            this.rlbRole.Size = new System.Drawing.Size(36, 22);
+            this.rlbRole.TabIndex = 3;
+            this.rlbRole.Text = "Role";
+            // 
+            // rlbActive
+            // 
+            this.rlbActive.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rlbActive.ForeColor = System.Drawing.Color.Teal;
+            this.rlbActive.Location = new System.Drawing.Point(51, 198);
+            this.rlbActive.Name = "rlbActive";
+            this.rlbActive.Size = new System.Drawing.Size(48, 22);
+            this.rlbActive.TabIndex = 4;
+            this.rlbActive.Text = "Active";
+            // 
+            // rlbLoginId
+            // 
+            this.rlbLoginId.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rlbLoginId.ForeColor = System.Drawing.Color.Teal;
+            this.rlbLoginId.Location = new System.Drawing.Point(51, 73);
+            this.rlbLoginId.Name = "rlbLoginId";
+            this.rlbLoginId.Size = new System.Drawing.Size(63, 22);
+            this.rlbLoginId.TabIndex = 1;
+            this.rlbLoginId.Text = "Login ID";
+            // 
+            // txtFname
+            // 
+            this.txtFname.AutoSize = false;
+            this.txtFname.Font = new System.Drawing.Font("Segoe UI", 10.25F);
+            this.txtFname.Location = new System.Drawing.Point(161, 33);
+            this.txtFname.Multiline = true;
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(198, 24);
+            this.txtFname.TabIndex = 1;
+            // 
+            // rlbPwd
+            // 
+            this.rlbPwd.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rlbPwd.ForeColor = System.Drawing.Color.Teal;
+            this.rlbPwd.Location = new System.Drawing.Point(51, 116);
+            this.rlbPwd.Name = "rlbPwd";
+            this.rlbPwd.Size = new System.Drawing.Size(70, 22);
+            this.rlbPwd.TabIndex = 2;
+            this.rlbPwd.Text = "Password";
+            // 
+            // rlbFname
+            // 
+            this.rlbFname.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rlbFname.ForeColor = System.Drawing.Color.Teal;
+            this.rlbFname.Location = new System.Drawing.Point(51, 33);
+            this.rlbFname.Name = "rlbFname";
+            this.rlbFname.Size = new System.Drawing.Size(79, 22);
+            this.rlbFname.TabIndex = 0;
+            this.rlbFname.Text = "First Name";
+            // 
+            // radGb2
+            // 
+            this.radGb2.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.radGb2.Controls.Add(this.btnSave);
+            this.radGb2.Controls.Add(this.ddlRole);
+            this.radGb2.Controls.Add(this.txtPassword);
+            this.radGb2.Controls.Add(this.txtLoginid);
+            this.radGb2.Controls.Add(this.btnNew);
+            this.radGb2.Controls.Add(this.ddlActive);
+            this.radGb2.Controls.Add(this.rlbRole);
+            this.radGb2.Controls.Add(this.rlbActive);
+            this.radGb2.Controls.Add(this.rlbLoginId);
+            this.radGb2.Controls.Add(this.rlbPwd);
+            this.radGb2.Controls.Add(this.txtFname);
+            this.radGb2.Controls.Add(this.rlbFname);
+            this.radGb2.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radGb2.ForeColor = System.Drawing.Color.Teal;
+            this.radGb2.HeaderText = "Add New User";
+            this.radGb2.Location = new System.Drawing.Point(526, 58);
+            this.radGb2.Name = "radGb2";
+            this.radGb2.Size = new System.Drawing.Size(461, 308);
+            this.radGb2.TabIndex = 3;
+            this.radGb2.Text = "Add New User";
+            // 
+            // rgvUsers
+            // 
+            this.rgvUsers.Location = new System.Drawing.Point(14, 21);
+            // 
+            // 
+            // 
+            this.rgvUsers.MasterTemplate.AllowAddNewRow = false;
+            this.rgvUsers.MasterTemplate.AllowCellContextMenu = false;
+            this.rgvUsers.MasterTemplate.AllowDeleteRow = false;
+            this.rgvUsers.MasterTemplate.AllowDragToGroup = false;
+            this.rgvUsers.MasterTemplate.AllowEditRow = false;
+            this.rgvUsers.MasterTemplate.AllowRowResize = false;
+            gridViewTextBoxColumn1.AllowFiltering = false;
+            gridViewTextBoxColumn1.AllowGroup = false;
+            gridViewTextBoxColumn1.AllowHide = false;
+            gridViewTextBoxColumn1.AllowReorder = false;
+            gridViewTextBoxColumn1.HeaderText = "User";
+            gridViewTextBoxColumn1.Multiline = true;
+            gridViewTextBoxColumn1.Name = "User";
+            gridViewTextBoxColumn1.Width = 60;
+            gridViewTextBoxColumn2.AllowFiltering = false;
+            gridViewTextBoxColumn2.AllowGroup = false;
+            gridViewTextBoxColumn2.AllowHide = false;
+            gridViewTextBoxColumn2.AllowReorder = false;
+            gridViewTextBoxColumn2.HeaderText = "Username";
+            gridViewTextBoxColumn2.Name = "username";
+            gridViewTextBoxColumn2.RowSpan = 25;
+            gridViewTextBoxColumn2.Width = 70;
+            gridViewTextBoxColumn3.HeaderText = "Role";
+            gridViewTextBoxColumn3.Name = "column2";
+            gridViewTextBoxColumn4.HeaderText = "Active";
+            gridViewTextBoxColumn4.Name = "column3";
+            this.rgvUsers.MasterTemplate.Columns.AddRange(new Telerik.WinControls.UI.GridViewDataColumn[] {
+            gridViewTextBoxColumn1,
+            gridViewTextBoxColumn2,
+            gridViewTextBoxColumn3,
+            gridViewTextBoxColumn4});
+            this.rgvUsers.MasterTemplate.ViewDefinition = tableViewDefinition1;
+            this.rgvUsers.Name = "rgvUsers";
+            this.rgvUsers.Size = new System.Drawing.Size(462, 271);
+            this.rgvUsers.TabIndex = 0;
+            this.rgvUsers.Text = "radGridView1";
+            // 
+            // radGb1
+            // 
+            this.radGb1.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.radGb1.Controls.Add(this.rgvUsers);
+            this.radGb1.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radGb1.ForeColor = System.Drawing.Color.Teal;
+            this.radGb1.HeaderText = " Users List";
+            this.radGb1.Location = new System.Drawing.Point(15, 52);
+            this.radGb1.Name = "radGb1";
+            this.radGb1.Size = new System.Drawing.Size(493, 314);
+            this.radGb1.TabIndex = 2;
+            this.radGb1.Text = " Users List";
             // 
             // frmUsers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1098, 419);
+            this.Controls.Add(this.radGb2);
+            this.Controls.Add(this.radGb1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmUsers";
             // 
@@ -45,6 +281,24 @@
             this.RootElement.ApplyShapeToControl = true;
             this.Text = "frmUsers";
             this.ThemeName = "Office2010Blue";
+            ((System.ComponentModel.ISupportInitialize)(this.btnSave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ddlRole)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtLoginid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnNew)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ddlActive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rlbRole)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rlbActive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rlbLoginId)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFname)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rlbPwd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rlbFname)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGb2)).EndInit();
+            this.radGb2.ResumeLayout(false);
+            this.radGb2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rgvUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGb1)).EndInit();
+            this.radGb1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
 
@@ -53,5 +307,21 @@
         #endregion
 
         private Telerik.WinControls.Themes.Office2010BlueTheme office2010BlueTheme1;
+        private Telerik.WinControls.UI.RadButton btnSave;
+        private Telerik.WinControls.UI.RadDropDownList ddlRole;
+        private Telerik.WinControls.UI.RadTextBox txtPassword;
+        private Telerik.WinControls.UI.RadTextBox txtLoginid;
+        private Telerik.WinControls.UI.RadButton btnNew;
+        private Telerik.WinControls.UI.RadDropDownList ddlActive;
+        private Telerik.WinControls.UI.RadLabel rlbRole;
+        private Telerik.WinControls.UI.RadLabel rlbActive;
+        private Telerik.WinControls.UI.RadLabel rlbLoginId;
+        private Telerik.WinControls.UI.RadTextBox txtFname;
+        private Telerik.WinControls.UI.RadLabel rlbPwd;
+        private Telerik.WinControls.Themes.Office2010BlueTheme office2010BlueTheme2;
+        private Telerik.WinControls.UI.RadLabel rlbFname;
+        private Telerik.WinControls.UI.RadGroupBox radGb2;
+        private Telerik.WinControls.UI.RadGridView rgvUsers;
+        private Telerik.WinControls.UI.RadGroupBox radGb1;
     }
 }
